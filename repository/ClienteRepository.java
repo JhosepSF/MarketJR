32bf4f241e9b21e8237b7f8b9ae5b7caaeee4938
JhosepSF <134757606+JhosepSF@users.noreply.github.com>
2024-04-24 16:05:38 -0500

    Delete repository directory

--- repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marketin.main.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.marketin.main.models.Cliente;
-
-public interface ClienteRepository extends CrudRepository <Cliente, String>
-{
-}
